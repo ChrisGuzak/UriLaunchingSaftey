a6ad75cdec1975163aef4e40853883a24e043a56
Chris Guzak <chris_guzak@msn.com>
2025-04-25 17:38:40 -0700

    Initial test project

---
+++ Tests/Tests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UriLaunchingSafetey
+{
+	TEST_CLASS(UseCases)
+    {
+        TEST_METHOD(DetectLocalOnlyUriSchemes)
+        {
+
+        }
+
+		TEST_METHOD(LaunchWithShellExecute)
+		{
+
+		}
+	};
+}
